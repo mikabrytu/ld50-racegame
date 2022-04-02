f29a8011e70bb6e902dabc11ec53e00328f78f47
Micael Brito <micael.brito@storytoys.com>
2022-04-02 11:19:42 -0300

    Add turbo to move system

--- Assets/Scripts/Components/CarComponent.cs
+++ Assets/Scripts/Components/CarComponent.cs
@@ -9,6 +9,7 @@ namespace Mikabrytu.LD50.Components
     {
         [SerializeField] private PathCreator pathCreator;
         [SerializeField] private float speed;
+        [SerializeField] private float turboMultiplier;
 
         private MoveSystem moveSystem;
 
@@ -19,6 +20,12 @@ namespace Mikabrytu.LD50.Components
 
         private void Update()
         {
+            if (Input.GetKeyDown(KeyCode.Space))
+                moveSystem.ActivateTurbo(turboMultiplier, true);
+            
+            if (Input.GetKeyUp(KeyCode.Space))
+                moveSystem.ActivateTurbo(turboMultiplier, false);
+            
             moveSystem.Move(speed);
         }
     }
--- Assets/Scripts/Systems/MoveSystem.cs
+++ Assets/Scripts/Systems/MoveSystem.cs
@@ -8,6 +8,8 @@ namespace Mikabrytu.LD50.Systems
         private Transform transform;
         private PathCreator pathCreator;
         private float pathFollowed;
+        private float multiplier;
+        private bool isTurbo;
 
         public MoveSystem(Transform transform, PathCreator pathCreator)
         {
@@ -19,9 +21,20 @@ namespace Mikabrytu.LD50.Systems
 
         public void Move(float speed)
         {
-            pathFollowed += speed * Time.deltaTime;
-            transform.position = pathCreator.path.GetPointAtDistance(pathFollowed);
-            transform.rotation = pathCreator.path.GetRotationAtDistance(pathFollowed);
+            if (isTurbo)
+                transform.Translate(Vector3.forward * (speed * multiplier) * Time.deltaTime);
+            else
+            {
+                pathFollowed += speed * Time.deltaTime;
+                transform.position = pathCreator.path.GetPointAtDistance(pathFollowed);
+                transform.rotation = pathCreator.path.GetRotationAtDistance(pathFollowed);
+            }
+        }
+
+        public void ActivateTurbo(float multiplier, bool activate)
+        {
+            isTurbo = activate;
+            this.multiplier = multiplier;
         }
     }
 }
